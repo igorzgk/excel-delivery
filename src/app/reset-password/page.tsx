d13dff7439556601c03ff2315a7b5f9074a094a6
igorzgk <igor.zagkorski@gmail.com>
2026-01-13 22:30:24 +0200

    Project updates

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from "react";
 import ResetPasswordClient from "./ResetPasswordClient";
 
+export const dynamic = "force-dynamic";
+
+
 export default function ResetPasswordPage() {
   return (
     <Suspense
