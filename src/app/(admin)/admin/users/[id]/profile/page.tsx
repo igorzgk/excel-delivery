c1ca0a13dc35a701da8fcbd415f9bdca350c2d77
igorzgk <igor.zagkorski@gmail.com>
2026-05-18 12:26:34 +0300

    Project updates

--- src/app/(admin)/admin/users/[id]/profile/page.tsx
+++ src/app/(admin)/admin/users/[id]/profile/page.tsx
@@ -51,7 +51,8 @@ type PublicHoliday =
   | "AUG_15"
   | "OCT_28"
   | "CHRISTMAS"
-  | "BOXING_DAY";
+  | "BOXING_DAY"
+  | "MAY_1";
 
 type ProfilePayload = {
   businessName: string;
@@ -128,6 +129,7 @@ const HOLIDAY_LABELS: Record<PublicHoliday, string> = {
   OCT_28: "28/10 (Εθνική εορτή)",
   CHRISTMAS: "25/12 (Χριστούγεννα)",
   BOXING_DAY: "26/12 (Επίσημη αργία)",
+  MAY_1: "01/05 (Εργατική Πρωτομαγιά)",
 };
 
 // ---------- Files / PDF types ----------
